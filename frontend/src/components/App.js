7e5595adb48bbc020b605c9b7e97a59b4121d9a0
Mersad Ajanovic <34665379+flamerged@users.noreply.github.com>
2021-03-27 18:48:42 +0100

    added basic route to home page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Initial home page</h1>;
+};
+
+export default Home;
